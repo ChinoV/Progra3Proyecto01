447d0b8f90438ac9f3997aacaae10b859fcfc842
Alfredo Valverde <chinochinese2@gmail.com>
2017-08-23 18:16:46 -0600

    Cola Tests
    also renamed fileUtilities to utilidades de archivos

--- Proyecto2Progra3/test/proyecto2progra3/ColaTest.java
+++ Proyecto2Progra3/test/proyecto2progra3/ColaTest.java
@@ -68,8 +68,6 @@ public class ColaTest<T> {
         Object objeto = null;
         Cola instance = new Cola();
         instance.Enqueue(objeto);
-        
-        
     }
 
     /**
@@ -79,8 +77,7 @@ public class ColaTest<T> {
     public void testDequeue() {
         System.out.println("Dequeue");
         Cola instance = new Cola();
-        //T Aux = (T) instance.getCabeza().getObjeto();
-        Object expResult = new Persona("3-1437-0004", 20, "a", "b", "c");
+        Object expResult = null;
         Object result = instance.Dequeue();
         assertEquals(expResult, result);
     }
@@ -104,7 +101,7 @@ public class ColaTest<T> {
     public void testIsEmpty() {
         System.out.println("IsEmpty");
         Cola instance = new Cola();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.IsEmpty();
         assertEquals(expResult, result);
     }
@@ -147,6 +144,61 @@ public class ColaTest<T> {
         assertEquals(expResult, result);
         
         
+    }
+    
+    /**
+     * Test of getString method, of class Cola.
+     */
+    @Test
+    public void generalTest() {
+        System.out.println("General Test");
+        
+        Lista<Persona> toFixPersonas = new Lista<Persona>(); 
+        toFixPersonas.Add(new Persona("3-1437-0004", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("E52461", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("3-1427-0004", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("E52261", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("2-1487-0004", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("1-1432-0004", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("1-1417-0004", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("2-1437-1004", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("1-1437-0224", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("E12461", 20, "a", "b", "c"));
+        toFixPersonas.Add(new Persona("E51461", 20, "a", "b", "c"));
+        toFixPersonas.brickSort(toFixPersonas);
+        
+        Lista<Persona> resultPersonas = new Lista<Persona>(); 
+        resultPersonas.Add(new Persona("1-1417-0004", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("1-1432-0004", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("1-1437-0224", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("2-1437-1004", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("2-1487-0004", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("3-1427-0004", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("3-1437-0004", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("E12461", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("E51461", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("E52261", 20, "a", "b", "c"));
+        resultPersonas.Add(new Persona("E52461", 20, "a", "b", "c"));
+            
+        
+        boolean expResult = true;
+        
+        Nodo Aux = toFixPersonas.getCabeza();
+        Nodo Aux2 = resultPersonas.getCabeza();
+        while (Aux != null)
+        {
+            Persona persona1 = (Persona)Aux.getObjeto();
+            Persona persona2 = (Persona)Aux2.getObjeto();
+            String cedula1 = persona1.getCedula();
+            String cedula2 = persona2.getCedula();
+            if (!cedula1.equals(cedula2))
+            {
+                expResult = false;
+            }
+            Aux = Aux.getSiguiente(); 
+            Aux2 = Aux2.getSiguiente(); 
+        }
+        assertEquals(expResult, true);
     }
     
 }
